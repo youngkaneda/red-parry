55944599dbe316463be34d405a4170b208e3aca6
shotaro kaneda <juanpablolvl99@gmail.com>
2020-05-04 18:09:36 -0300

    Adding heroku files.

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import serveStatic from 'serve-static';
+
+const path = require('path');
+
+const app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
