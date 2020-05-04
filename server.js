215f9ac4309e6d5b588f56a909a10edb1cd7fd41
shotaro kaneda <juanpablolvl99@gmail.com>
2020-05-04 18:12:54 -0300

    Fixing import problem.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import serveStatic from 'serve-static';
+const express = require('express');
+const serveStatic = require('serve-static');
 
 const path = require('path');
 
