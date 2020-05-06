0a464ab3e6fcde75794d7222523cf97dfd9f3067
shotaro kaneda <juanpablolvl99@gmail.com>
2020-05-06 03:13:37 -0300

    Changing youtube scope.

--- props.js
+++ props.js
@@ -1,5 +1,5 @@
 export default {
-    scopes: ['openid', 'profile', 'email', 'https://www.googleapis.com/auth/youtube'].join(' '),
+    scopes: ['openid', 'profile', 'email', 'https://www.googleapis.com/auth/youtube.readonly'].join(' '),
     clientId: process.env.VUE_APP_CLIENT_ID,
     clientSecret: process.env.VUE_APP_CLIENT_SECRET,
     redirectURI: process.env.VUE_APP_REDIRECT_URI,
