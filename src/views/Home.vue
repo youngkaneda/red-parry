e9cd7b5d1c9ae1a795b62624e2f60b75beba951f
shotaro kaneda <juanpablolvl99@gmail.com>
2020-05-14 16:04:41 -0300

    Adding site logo and changing title.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,10 @@
 <template>
     <div>
         <div class="md-layout md-gutter md-alignment-center-left" style="padding: 0.5% 0 0.5% 0; background-color: black;">
-            <div class="md-layout-item md-size-20" style="display: flex; align-content: center !important;">
-                <span class="md-subheading"
-                    @click="() => { $router.push('/') }"
-                    style="cursor: pointer; margin: 10px 7px 0 20px;"
-                >
-                    Red Parry
-                </span>
+            <div class="md-layout-item md-size-10" style="padding: 0 0 0 1.5%">
+                <img src="../assets/rpw.png" alt="" @click="() => { $router.push('/') }" style="cursor: pointer">
+            </div>
+            <div class="md-layout-item md-size-10" style="display: flex; align-content: center !important; padding-left:0">
                 <md-button class="md-icon-button" style="margin-top: 1%" size="sm" @click="() => $router.push('/edit')">
                     <md-icon>add</md-icon>
                 </md-button>
